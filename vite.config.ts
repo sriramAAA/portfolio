bba0a6d6837ac9ef761ea5b3143c81d77999323f
Sriram A <sriram.coder2004@gmail.com>
2026-01-25 02:37:04 +0530

    fix :update my linkedin link

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/", // ✅ Add this line (your repo name)
+  // base: "/portfolio/",
+   // ✅ Add this line (your repo name)
   server: {
     host: "::",
     port: 8080,
